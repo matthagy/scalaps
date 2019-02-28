8126f88338d2df23d3c26396620174345d152fbc
Matt Hagy <matthew.hagy@gmail.com>
2019-02-27 21:04:26 -0800

    fixed copyright

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Matt Hagy <matthew.hagy@gmail.com>
+# Copyright 2019 Matt Hagy <matthew.hagy@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the “Software”), to deal in
